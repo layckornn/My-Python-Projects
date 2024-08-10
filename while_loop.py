f456e06560db48a878e378cf2874d059bf0c9a19
layckornn <lekanadeogun2015@gmail.com>
2024-08-10 18:31:19 +0100

    commit

---
+++ while_loop.py
@@ -0,0 +1,5 @@
+#while condition/expression: 
+#    statement(s)
+
+#In summary, This or these statement will be executed only when the condition is true. and if the condition is false, exit from while loop
+
